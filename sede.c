69d58912932a870ef154795109e9f4c0e0b71a39
ahuacate15 <carlos.itca@gmail.com>
2020-04-25 13:50:01 -0600

    Agregar prototipos de funciones

--- sede.c
+++ sede.c
@@ -24,6 +24,18 @@ typedef struct listaSede {
     Sede *ultimo;
 } ListaSede;
 
+/*-----------prototipos de funciones estandar----------*/
+ListaSede *initListSede(); 
+Sede *initSede();
+Sede *appendSede(ListaSede *lista, Sede *sede); //agregar una sede al final de la lista, retorna el nodo creado
+int isEmptyListSede(ListaSede *lista); //compruebo si la lista se encuentra vacia
+Sede *getSede(ListaSede *lista, int id); //obtengo la sede segun el id
+int removeSede(ListaSede *lista, int id); //elimino una sede segun el id
+
+/*--------prototipo de funciones para interfaz --------*/
+void requestNewSede(ListaSede *lista); //solicito los datos de una sede y la agrego a la lista
+void printListSedes(ListaSede *lista); 
+
 ListaSede *initListSede() {
     ListaSede *lista = malloc(sizeof(ListaSede));
     lista->total = 0;
