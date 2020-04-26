0353b9c9e1ae36c8a22b630fdbc4b9e26a049b5c
ahuacate15 <carlos.itca@gmail.com>
2020-04-25 21:42:08 -0600

    limpiar el buffer despues de leer una linea

--- texto.c
+++ texto.c
@@ -6,10 +6,12 @@ char *getLine(int max) {
     char c, *buffer = malloc(sizeof(char) * max);
     int i = 0;
 
-    while((c = getchar()) != '\n' && i < max - 1) 
+	//la lectura de caracteres se detiene cuando se presiona enter o cuando se llega al maximo de caracteres permitidosd
+    while((c = getchar()) != '\n' && i < max - 1)
         buffer[i++] = c;
     
     buffer[i] = '\0';
+    fflush(stdin);
 
     return buffer;
 
